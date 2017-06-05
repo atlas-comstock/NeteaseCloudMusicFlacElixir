6c04a0d72bfe34552330ca706b888f033d5dfbcb
YongHaoHu <christopherwuy@gmail.com>
2017-06-05 10:58:44 +0800

    Success get song id.

--- lib/downloader.ex
+++ lib/downloader.ex
@@ -2,22 +2,61 @@ defmodule Downloader do
   @moduledoc """
   Documentation for Downloader.
   """
+  @baiduURL "http://sug.music.baidu.com/info/suggestion"
 
   def run(argv) do
-    song_list = argv
+  argv
     |> parse_args
     |> to_string
-    |> fetch_song_list()
+    |> fetch_song_list
     |> download_songs
   end
 
-  def download_songs([]) do
-    []
+  def get_song_info(song_name) do
+    song_name = song_name |> to_string
+    query = 
+      %{"word" => song_name, "version" => 2, "from" => 0}
+      |> URI.encode_query()
+
+    IO.puts @baiduURL <> "?#{query}"
+    case( 
+      @baiduURL <> "?#{query}"
+      |> HTTPotion.get()
+      |> Map.fetch(:body)
+    ) do
+      {:ok, body} -> body
+      {:error, reason} -> IO.puts "error get_song_info #{reason}"
+    end
   end
 
+  def get_song_id({:error, reason}) do
+    IO.puts "error get_song_id #{reason}"
+  end
+
+  def get_song_id({:ok, song_info}) do
+    song_id_info = cond do
+      song_info == nil -> IO.puts "song_info nil"
+      song_info["data"] == nil -> IO.puts "song_info[data] nil"
+      true -> song_info["data"]["song"] |> Enum.at(0) |> Map.fetch("songid")
+    end
+
+    case song_id_info do
+      {:ok, song_id} -> song_id |> IO.puts
+      {:error, reason} -> IO.puts "error get_song_id #{reason}"
+    end
+  end
+
+  def get_download_url() do
+  end
+
+  def download_songs([]) do [] end
+
   def download_songs([song_name | song_list]) do
-    IO.inspect song_name
-    download_songs(song_list)
+    IO.puts "song_name #{song_name}"
+    get_song_info(song_name)
+    |> Poison.decode
+    |> get_song_id
+    # download_songs(song_list)
   end
 
   def download_songs(song_name) do
@@ -28,7 +67,7 @@ defmodule Downloader do
     IO.puts url
     url
     |> String.replace("#", "")
-    HTTPotion.get(url)
+    |> HTTPotion.get!()
     |> handle_response()
     |> ( &Regex.scan(~r/<ul class=\"f-hide\">(.*?)<\/ul>/sm, &1) ).()
     |> to_string
@@ -41,6 +80,10 @@ defmodule Downloader do
 
   def handle_response(%{body: body, headers: _, status_code: _}) do
     body
+  end
+
+  def handle_response(message) do
+   raise  message
   end
 
   def parse_args(argv) do
--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule Downloader.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpotion, "~> 3.0.2"}
+      {:httpotion, "~> 3.0.2"},
+      {:poison, "~> 3.0", overwrite: true},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
